063043d68faa3b1f2ea760c9fbeffa9a4989abae
slavikovbasa <slavikovbasa@gmail.com>
2016-06-07 23:18:57 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,33 @@
-/**
- * Created by 1 on 07/06/16.
- */
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+    public static final float WINDOW_WIDTH = 640, WINDOW_HEIGHT = 480;
+    public static GraphicsContext graphics;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Ganjubassed");
+
+        Pane rootNode = new Pane();
+
+        Scene gameScene = new Scene(rootNode, WINDOW_WIDTH, WINDOW_HEIGHT );
+
+        Canvas gameCanvas = new Canvas(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        graphics = gameCanvas.getGraphicsContext2D();
+
+        rootNode.getChildren().add(gameCanvas);
+
+        primaryStage.setScene(gameScene);
+        primaryStage.show();
     }
 }
