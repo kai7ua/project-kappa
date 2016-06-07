8e88c55c35382dbe03e40bb30e83a5be52decd04
kai7ua <kai7@ukr.net>
2016-06-07 22:35:51 +0300

    1

---
+++ src/Counter.java
@@ -0,0 +1,8 @@
+/**
+ * Created by kai on 6/7/16.
+ */
+public class Counter {
+    public void count(int start, int end){
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-/**
- * Created by 1 on 07/06/16.
- */
 public class Main {
 
 }
