00aa11873fbeb1ea47c269e4d459fd950932c123
slavikovbasa <slavikovbasa@gmail.com>
2016-06-07 22:38:56 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@
  * Created by 1 on 07/06/16.
  */
 public class Main {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
 
+        counter.count(0, 10);
+        counter.count(20, 30);
+        counter.count(50, 60);
+
+    }
 }
